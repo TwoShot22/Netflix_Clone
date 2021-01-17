5ece43f778ccb0060e4b8d46e77c35849f889ac9
Teddy <teddy@amanda.co.kr>
2021-01-17 21:24:02 +0900

    홈 화면, 컨텐츠 정보 화면, 프로필 화면, 바텀 네비게이션 추가

---
+++ lib/main.dart
@@ -0,0 +1,41 @@
+import 'package:flutter/material.dart';
+import 'package:netflix_flutter/screen/home_screen.dart';
+import 'package:netflix_flutter/screen/more_screen.dart';
+import 'package:netflix_flutter/widget/bottom_bar.dart';
+
+void main() {
+  runApp(MyApp());
+}
+
+class MyApp extends StatefulWidget {
+  _MyAppState createState() => _MyAppState();
+}
+
+class _MyAppState extends State<MyApp> {
+  TabController controller;
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+        title: 'Netflix',
+        theme: ThemeData(
+            brightness: Brightness.dark,
+            primaryColor: Colors.black,
+            accentColor: Colors.white),
+        home: DefaultTabController(
+          length: 4,
+          child: Scaffold(
+            body: TabBarView(
+              physics: NeverScrollableScrollPhysics(),
+              children: [
+                HomeScreen(),
+                Container(child: Center(child: Text('search'))),
+                Container(child: Center(child: Text('save'))),
+                MoreScreen(),
+              ],
+            ),
+            bottomNavigationBar: Bottom(),
+          ),
+        ));
+  }
+}
